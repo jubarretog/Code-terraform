40f6164e9797c8087d3c7c11ddd2b04386b51d20
KryptoCoder <64144710+jubarretog@users.noreply.github.com>
2024-05-15 18:26:55 -0500

    Update main.tf
    Updated code for creating another repository with branches

--- main.tf
+++ main.tf
@@ -15,21 +15,32 @@ provider "github" {
 resource "github_repository" "example" {
   name        = "example"
   description = "My awesome codebase"
-
+  auto_init   = true
   visibility = "public"
-
 }
+
+resource "time_sleep" "wait_for_repository" {
+  depends_on = [github_repository.example]
+  create_duration = "20s"
+}
+
 resource "github_branch" "development" {
   repository = "example"
   branch     = "development"
+  source_branch = "master"
+  depends_on = [time_sleep.wait_for_repository]
 }
 
 resource "github_branch" "testing" {
   repository = "example"
   branch     = "testing"
+  source_branch = "master"
+  depends_on = [time_sleep.wait_for_repository]
 }
 
 resource "github_branch" "arquitics" {
   repository = "example"
   branch     = "arquitics"
+  source_branch = "master"
+  depends_on = [time_sleep.wait_for_repository]
 }
